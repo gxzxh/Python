131b2e0c177a2e17b6658396939723dcfa6a8bc3
新华 <lx1672@163.com>
2023-02-09 11:04:57 +0800

    leetcode习题

---
+++ 两数之和.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+    hashmap = {}
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[nums[i]] = i
+    return []
