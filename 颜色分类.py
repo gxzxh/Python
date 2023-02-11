9dd29b5973130b3135b0716b552dea0b22c883f6
新华 <lx1672@163.com>
2023-02-11 23:23:57 +0800

    Leetcode习题

---
+++ 颜色分类.py
@@ -0,0 +1,27 @@
+def sortColors(nums):
+    # 方法一
+    i, j = 0, len(nums) - 1
+    count = 0
+    while i <= j:
+        if nums[i] == 2:
+            nums[i], nums[j] = nums[j], nums[i]
+            j -= 1  # 数值 2 交换到尾部
+        elif nums[i] == 1:
+            nums[i] = 0  # 写入0
+            count += 1  # 计算 1 的个数
+            i += 1
+        else:
+            i += 1  # 0 只移动指针
+    for index in range(j, j - count, -1):
+        nums[index] = 1  # 写入 count 个 1
+    # 方法二
+    n = len(nums)
+    ptr = 0
+    for i in range(n):
+        if nums[i] == 0:
+            nums[i], nums[ptr] = nums[ptr], nums[i]
+            ptr += 1
+    for i in range(ptr, n):
+        if nums[i] == 1:
+            nums[i], nums[ptr] = nums[ptr], nums[i]
+            ptr += 1
